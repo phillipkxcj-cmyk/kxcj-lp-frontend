f3edef91005b6a5b04a89bc95ba2a8cc884d6c70
Robert-Lark <robert.lark87@gmail.com>
2022-10-03 14:28:32 -0700

    homepage

--- kxcj-lp-front-end/src/App.tsx
+++ kxcj-lp-front-end/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
 import './styles.css';
+import logo from '../src/assets/logo_white_2.png'
+
 
 function App() {
   return (
     <div className="homepage">
-      
+      <div className="≈">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="buttons">
+          <button className="button-5">Listen Now</button>
+          <button className="button-5">Donate</button>
+          <button className="button-5">Get Involved</button>
+        </div>
+      </div>
     </div>
   );
 }
