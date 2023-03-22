c139eaabf589fbfd9c67de786afcbef75c9970c5
Robert-Lark <robert.lark87@gmail.com>
2023-03-22 15:00:05 -0700

    Complete footer

--- kxcj-lp-front-end/src/lib/Footer.tsx
+++ kxcj-lp-front-end/src/lib/Footer.tsx
@@ -1,28 +1,34 @@
 import "../styles/footer.css";
 
-
 function Footer() {
   return (
     <div className="footer">
       <div className="container">
-        <div className="box none">
-          <div className="box-content"></div>
-        </div>
+        <a role="button" href="https://www.facebook.com/KXCJ1057/">
+          <div className="box">
+            <div className="box-content"></div>
+          </div>
+        </a>
+        <div className="finalTwo">
         <div className="box">
           <div className="box-content-snail-mail"></div>
         </div>
         <div className="box">
           <div className="box-content-phone"></div>
         </div>
-        <div className="box">
-          <div className="box-content-email" id="gmail"></div>
+        
+          <div className="box">
+            <div className="box-content-email" id="gmail"></div>
+          </div>
+          <div className="box">
+            <div className="box-content-radio" id="radio"></div>
+          </div>
         </div>
-        <div className="box">
-          <div className="box-content-radio" id="radio"></div>
-        </div>
-        <div className="box none">
-          <div className="box-content none"></div>
-        </div>
+        <a role="button" href="https://www.facebook.com/KXCJ1057/">
+          <div className="box">
+            <div className="box-content"></div>
+          </div>
+        </a>
       </div>
     </div>
   );
