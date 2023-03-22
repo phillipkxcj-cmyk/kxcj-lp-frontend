fbbb6d5967cffc02424f641c48c7b3c874cf39e2
Robert-Lark <robert.lark87@gmail.com>
2023-03-22 11:44:06 -0700

    position footer

--- kxcj-lp-front-end/src/lib/Footer.tsx
+++ kxcj-lp-front-end/src/lib/Footer.tsx
@@ -1,12 +1,11 @@
-import Footer2 from "../assets/footer/Footer2.png";
 import "../styles/footer.css";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <img src={Footer2} />
-        </div>
-    );
+  return (
+    <div className="footer">
+      <div className="container"></div>
+    </div>
+  );
 }
 
 export default Footer;
