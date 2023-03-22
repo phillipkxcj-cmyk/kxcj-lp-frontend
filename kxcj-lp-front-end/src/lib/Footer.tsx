e92e2c644b8042539030a22b0b52bac460e7719d
Robert-Lark <robert.lark87@gmail.com>
2023-03-22 14:40:19 -0700

    Footer hover state 1

--- kxcj-lp-front-end/src/lib/Footer.tsx
+++ kxcj-lp-front-end/src/lib/Footer.tsx
@@ -1,9 +1,29 @@
 import "../styles/footer.css";
 
+
 function Footer() {
   return (
     <div className="footer">
-      <div className="container"></div>
+      <div className="container">
+        <div className="box none">
+          <div className="box-content"></div>
+        </div>
+        <div className="box">
+          <div className="box-content-snail-mail"></div>
+        </div>
+        <div className="box">
+          <div className="box-content-phone"></div>
+        </div>
+        <div className="box">
+          <div className="box-content-email" id="gmail"></div>
+        </div>
+        <div className="box">
+          <div className="box-content-radio" id="radio"></div>
+        </div>
+        <div className="box none">
+          <div className="box-content none"></div>
+        </div>
+      </div>
     </div>
   );
 }
