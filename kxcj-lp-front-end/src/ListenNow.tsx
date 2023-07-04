cd833ad8feabae46613a4e0d599f7910b676e107
Robert-Lark <robert.lark87@gmail.com>
2023-07-04 14:14:48 -0700

    Adjustments to the width of tockify cal and donate mobile

--- kxcj-lp-front-end/src/ListenNow.tsx
+++ kxcj-lp-front-end/src/ListenNow.tsx
@@ -44,6 +44,7 @@ function ListenNow() {
             data-tockify-component="calendar"
             data-tockify-calendar="kxcjfm"
             data-tockify-height="550px"
+            data-tockify-max-width="850px"
           ></div>
         </div>
         <div className="listen-footer">
