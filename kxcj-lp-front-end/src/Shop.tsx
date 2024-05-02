11bbf9ef067b566e343603a56f26d35b1055bfbe
Robert-Lark <robert.lark87@gmail.com>
2024-05-02 14:57:46 -0700

    Add merch

--- kxcj-lp-front-end/src/Shop.tsx
+++ kxcj-lp-front-end/src/Shop.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import "./styles/test.css";
-import Paypal from "./assets/PayPal_Plank_2.png";
+
 import Scroll from "./assets/T-shirt-Scroll.png";
-import RedShirt from "./assets/Red_Shirt.png";
-import YellowShirt from "./assets/Yellow_Shirt.png";
-import WhiteShirt from "./assets/White_Shirt.png";
-import GreyShirt from "./assets/Grey_Shirt.png";
-import BlueShirt from "./assets/Blue_Shirt.png";
+import Merch from "./assets/Merch_tiny.png";
+
 import SideButtons from "./lib/SideButtons";
 import Footer from "./lib/Footer";
 
@@ -19,47 +16,17 @@ function Shop() {
       <div className="sidebar-test">
         <SideButtons back currentPage={location.pathname} />
       </div>
-      <div className="content-test">
-        <div className="component-test-shop">
-          {" "}
-          <img src={Scroll} alt="scroll" />
-        </div>
-        <div className="shirts-1-row">
+      <div className="merch-test">
+        
+        <div className="merch-1-row">
           <a
             role="button"
             href="https://www.paypal.com/donate?hosted_button_id=D4VVLZXCKBVPS"
             className="button-wood-plank"
           >
-            <img src={RedShirt} alt="archive button" id="tee" />
+            <img src={Merch} alt="archive button" id="tee" />
           </a>
-          <a
-            role="button"
-            href="https://www.paypal.com/donate?hosted_button_id=D4VVLZXCKBVPS"
-            className="button-wood-plank"
-          >
-            <img src={YellowShirt} alt="archive button" id="tee" />
-          </a>
-          <a
-            role="button"
-            href="https://www.paypal.com/donate?hosted_button_id=D4VVLZXCKBVPS"
-            className="button-wood-plank"
-          >
-            <img src={WhiteShirt} alt="archive button" id="tee" />
-          </a>
-          <a
-            role="button"
-            href="https://www.paypal.com/donate?hosted_button_id=D4VVLZXCKBVPS"
-            className="button-wood-plank"
-          >
-            <img src={GreyShirt} alt="archive button" id="tee" />
-          </a>
-          <a
-            role="button"
-            href="https://www.paypal.com/donate?hosted_button_id=D4VVLZXCKBVPS"
-            className="button-wood-plank"
-          >
-            <img src={BlueShirt} alt="archive button" id="tee" />
-          </a>
+         
         </div>
         <div className="donate-footer">
           <Footer />
