5b2b7946b322b22e1ceb5b5333e8cbaabe956bb0
Robert-Lark <robert.lark87@gmail.com>
2024-05-27 14:56:49 -0700

    increase submitPSA image

--- kxcj-lp-front-end/src/SubmitPSA.tsx
+++ kxcj-lp-front-end/src/SubmitPSA.tsx
@@ -6,35 +6,35 @@ import Scroll from "./assets/Scrolls/Scroll_p.png";
 import SideButtons from "./lib/SideButtons";
 
 function Modal(props: any) {
-    const { show, setShow } = props;
-    if (!show) return null;
-    return (
-      <div className="modal">
-        <h1>Example Announcement</h1>
-        <p>
-          <strong>Contact:</strong> Insert the contact name, institution name,
-          email address and telephone number
-        </p>
-        <p>
-          <strong>For use through:</strong> End date of announcement
-        </p>
-        <p>
-          <strong>Length:</strong> 0:30 - Indicates announcement runs for thirty
-          seconds
-        </p>
-        <p>
-          <strong>Content:</strong> One of the most important activities to share
-          with your baby is reading. That's why the illinois valley public library
-          is offering the "Born to read" program for teen parents. Tutors provide
-          coaching, a video offers tips, and librarians distribute books and other
-          materials. The program is made possible through a grant from the four
-          way community foundation. for more information, or to be a volunteer,
-          call (505) 555-1234.
-        </p>
-        <button onClick={() => setShow(!show)}>Close</button>
-      </div>
-    );
-  }
+  const { show, setShow } = props;
+  if (!show) return null;
+  return (
+    <div className="modal">
+      <h1>Example Announcement</h1>
+      <p>
+        <strong>Contact:</strong> Insert the contact name, institution name,
+        email address and telephone number
+      </p>
+      <p>
+        <strong>For use through:</strong> End date of announcement
+      </p>
+      <p>
+        <strong>Length:</strong> 0:30 - Indicates announcement runs for thirty
+        seconds
+      </p>
+      <p>
+        <strong>Content:</strong> One of the most important activities to share
+        with your baby is reading. That's why the illinois valley public library
+        is offering the "Born to read" program for teen parents. Tutors provide
+        coaching, a video offers tips, and librarians distribute books and other
+        materials. The program is made possible through a grant from the four
+        way community foundation. for more information, or to be a volunteer,
+        call (505) 555-1234.
+      </p>
+      <button onClick={() => setShow(!show)}>Close</button>
+    </div>
+  );
+}
 
 function SubmitPSAGood() {
   const location = useLocation();
@@ -58,8 +58,6 @@ function SubmitPSAGood() {
           "_9f0M6ZsHv1G2xiIL"
         )
         .then((result) => {
-          
-          
           alert("successfull form submission");
         })
         .catch((error) => {
@@ -78,73 +76,77 @@ function SubmitPSAGood() {
         <div className="component-test">
           <img
             src={Scroll}
-            style={{ maxWidth: "100%", height: "auto"}}
+            style={{ maxWidth: "100%", height: "auto" }}
             alt="scroll"
           />
-          <button className="example-psa-button" onClick={() => setShow(!show)}>
-            View an example PSA here
-          </button>
         </div>
-        <Modal show={show} setShow={setShow}/>
+        <Modal show={show} setShow={setShow} />
         <div className="component-test-form">
           <form onSubmit={handleSubmit} ref={form}>
-          <label className="form-label">
-            Name:
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              name="name"
-            />
-          </label>
-          <label className="form-label">
-            Email:
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              name="email"
-            />
-          </label>
-          <label className="form-label">
-            Use Through Date:
-            <input
-              type="text"
-              value={useThrough}
-              onChange={(e) => setUseThrough(e.target.value)}
-              name="useThroughDate"
-            />
-          </label>
-          <div className="radio-label">
+            <div className="first-block">
+              <label className="form-label">
+                Name:
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  name="name"
+                />
+              </label>
+              <label className="form-label">
+                Email:
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  name="email"
+                />
+              </label>
+            </div>
+            <div className="second-block">
+              <label className="form-label">
+                Use Through Date:
+                <input
+                  type="text"
+                  value={useThrough}
+                  onChange={(e) => setUseThrough(e.target.value)}
+                  name="useThroughDate"
+                />
+              </label>
+
+              <label className="form-label">
+                PSA Length:
+                <input
+                  id="ten"
+                  type="text"
+                  value={time10}
+                  onChange={(e) => setTime10(e.target.value)}
+                  name="psaTime"
+                  placeholder="10s or 30s psa"
+                />
+              </label>
+            </div>
             <label className="form-label">
-              PSA Length:
-              <input
-                id="ten"
-                type="text"
-                value={time10}
-                onChange={(e) => setTime10(e.target.value)}
-                name="psaTime"
-                placeholder="Please specify if you would like a 10s or 30s psa"
+              Your Announcement:
+              <textarea
+                rows={10}
+                cols={50}
+                name="announcement"
+                value={announcement}
+                className="form-textarea"
+                onChange={(e) => setAnnouncement(e.target.value)}
               />
             </label>
-          </div>
-          <label className="form-label">
-            Your Announcement:
-            <textarea
-              rows={10}
-              cols={50}
-              name="announcement"
-              value={announcement}
-              className="form-textarea"
-              onChange={(e) => setAnnouncement(e.target.value)}
-            />
-          </label>
 
             <button type="submit">Submit</button>
           </form>
         </div>
-
-        <div className="component-test"></div>
+        <div className="component-test">
+          <button className="example-psa-button" onClick={() => setShow(!show)}>
+            View an example PSA here
+          </button>
+        </div>
+        
       </div>
     </div>
   );
