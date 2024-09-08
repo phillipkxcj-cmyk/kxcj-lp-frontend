73c20c066f44b0e25262bcb1686503ca2b4b2121
Robert-Lark <robert.lark87@gmail.com>
2024-09-08 08:37:30 -0700

    Add radio buttons to grab orientation information

--- kxcj-lp-back-end/schemas/whatsOn.js
+++ kxcj-lp-back-end/schemas/whatsOn.js
@@ -1,12 +1,12 @@
 export default {
     name: "whatsOn",
     title: "What's On",
-    type: "document", // Use 'document' or 'object' to define the schema
+    type: "document",
     fields: [
       {
         name: "image",
         title: "Image",
-        type: "image", // Define the image field
+        type: "image",
         options: {
           hotspot: true, // Enable image hotspot
         },
@@ -14,10 +14,24 @@ export default {
       {
         name: "caption",
         type: "string",
-        title: "Caption",
+        title: "Caption (Optional)",
         options: {
           isHighlighted: true, // Make this field easily accessible
         },
+      },
+      {
+        name: "orientation",
+        title: "Image Orientation (Required)",
+        type: "string",
+        description: "Select whether the image is vertical or horizontal",
+        options: {
+          list: [
+            { title: "Vertical", value: "vertical" },
+            { title: "Horizontal", value: "horizontal" },
+          ], // Radio button choices
+          layout: "radio", // Renders as radio buttons
+        },
+        validation: Rule => Rule.required().error('You must select an image orientation (Vertical or Horizontal)'),
       },
     ],
   };
