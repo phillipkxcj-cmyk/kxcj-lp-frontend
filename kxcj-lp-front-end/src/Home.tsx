033f7e7671fcf1d3b4582402bd1b7f4714700341
Robert-Lark <robert.lark87@gmail.com>
2024-12-01 11:38:50 -0800

    Add mobile styles for home button-4

--- kxcj-lp-front-end/src/Home.tsx
+++ kxcj-lp-front-end/src/Home.tsx
@@ -8,15 +8,15 @@ import { useQuery, gql } from "@apollo/client";
 import ErrorState from "./lib/ErrorState";
 
 const GET_INVOVLED_QUERY = gql`
-query GetHome {
-  allHome {
-    image {
-      asset {
-        url
+  query GetHome {
+    allHome {
+      image {
+        asset {
+          url
+        }
       }
     }
   }
-}
 `;
 
 function Home() {
@@ -36,32 +36,57 @@ function Home() {
   };
 
   return (
-    <div className="homepage">
-      <div className="landscape-message">
-        Please rotate your device to portrait mode.
-      </div>
-      <div className="in-cont">
-        <div className="logo">
-          <img src={logoImage} alt="logo" />
+    <>
+      <div className="homepage">
+        <div className="landscape-message">
+          Please rotate your device to portrait mode.
         </div>
-        <div className="buttons-home" id="amatic">
-          <a href={isMobile ? "/" : "/listenNow"}>
-            <img
-              src={ListenNow}
-              id="plank"
-              alt="menu button"
-              onClick={openNewWindow}
-            />
-          </a>
-          <a href="/donate" className="donatePlank">
-            <img src={Donate} id="plank" alt="menu button" />
-          </a>
-          <a href="/getInvolved" className="getInvolvedPlank" id="lastChild">
-            <img src={GetInvolved} id="plank" alt="menu button" />
-          </a>
+        <div className="in-cont">
+          <div className="logo">
+            <img src={logoImage} alt="logo" />
+          </div>
+          <div className="buttons-home" id="amatic">
+            <a href={isMobile ? "/" : "/listenNow"}>
+              <img
+                src={ListenNow}
+                id="plank"
+                alt="menu button"
+                onClick={openNewWindow}
+              />
+            </a>
+            <a href="/donate" className="donatePlank">
+              <img src={Donate} id="plank" alt="menu button" />
+            </a>
+            <a href="/getInvolved" className="getInvolvedPlank" id="lastChild">
+              <img src={GetInvolved} id="plank" alt="menu button" />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="homepage-Mobile">
+        <div className="landscape-message">
+          Please rotate your device to portrait mode.
+        </div>
+        <div className="in-cont">
+          <div className="logo">
+            <img src={logoImage} alt="logo" />
+          </div>
+          <div className="buttons-home" id="amatic">
+            <a href={isMobile ? "/" : "/listenNow"}>
+              <img
+                src={ListenNow}
+                id="plank"
+                alt="menu button"
+                onClick={openNewWindow}
+              />
+            </a>
+            <a href="/donate" className="donatePlank">
+              <img src={Donate} id="plank" alt="menu button" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
